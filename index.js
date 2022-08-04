348b70e3e24443f44929f96934fbf14fc3d01648
iftekharzeeon <1805038@ugrad.cse.buet.ac.bd>
2022-08-05 03:05:26 +0600

    Phase 2 Almost done

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({
 
 
 // register view engine
-//app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
 
 app.use('/', routes);
 
@@ -23,4 +23,24 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.sendFile('./views/index.html', {root : __dirname});
+});
+
+app.get('/searchItem', (req, res) => {
+    res.sendFile('./views/searchItem.html', {root : __dirname});
+});
+
+app.get('/musicItem', (req, res) => {
+    res.sendFile('./views/musicItem.html', {root : __dirname});
+});
+
+app.get('/newsItem', (req, res) => {
+    res.sendFile('./views/newsItem.html', {root : __dirname});
+});
+
+app.get('/mapView', (req, res) => {
+    res.sendFile('./views/mapView.html', {root : __dirname});
+});
+
+app.get('/map', (req, res) => {
+    res.sendFile('./views/index_fabiha.html', {root : __dirname});
 });
--- routes/routes.js
+++ routes/routes.js
@@ -7,5 +7,7 @@ const router = express.Router();
 
 router.post('/api/getSearch/', bodyParser, searchController.searchEngine);
 
+router.post('/api/newsSearch', bodyParser, searchController.newsSearch);
+
 
 module.exports = router;
